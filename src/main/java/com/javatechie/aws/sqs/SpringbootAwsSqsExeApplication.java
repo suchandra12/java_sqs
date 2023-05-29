6931ef4c7a2d86747604cd6ac7eb97efae80ff2d
suchandra12 <suchandrareddy12@gmail.com>
2023-05-29 09:31:18 +0530

    first commit

--- src/main/java/com/javatechie/aws/sqs/SpringbootAwsSqsExeApplication.java
+++ src/main/java/com/javatechie/aws/sqs/SpringbootAwsSqsExeApplication.java
@@ -32,7 +32,7 @@ public class SpringbootAwsSqsExeApplication {
         queueMessagingTemplate.send(endpoint, MessageBuilder.withPayload(message).build());
     }
 
-    @SqsListener("javatechie-queue")
+   @SqsListener("javaQueue") // give your queue name here 
     public void loadMessageFromSQS(String message)  {
         logger.info("message from SQS Queue {}",message);
     }
